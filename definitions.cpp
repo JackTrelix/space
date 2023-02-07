eb406124282093e45a0bd795e5d9aaf11dc4ad70
Jacob Fewell <45701961+JackTrelix@users.noreply.github.com>
2023-02-07 12:26:05 -0800

    Update definitions.cpp

--- definitions.cpp
+++ definitions.cpp
@@ -20,7 +20,6 @@ std::vector<std::string> rowTen = { "\t", "\t", "\t", "\t", "\t", "\t", "\t", "\
 
 //funciton definitions
 std::string WordGenerator() {
-	std::vector<char> alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 	std::string word;
 	int counter = 2 + std::rand() % 5;
 	while (counter > 0) {
@@ -39,16 +38,16 @@ std::string WordGenerator() {
 
 void DrawBoard() {
 
-	rowOne[std::rand() % 10] = "*" + allStarsNames[0];
-	rowTwo[std::rand() % 10] = "*" + allStarsNames[1];
-	rowThree[std::rand() % 10] = "*" + allStarsNames[2];
-	rowFour[std::rand() % 10] = "*" + allStarsNames[3];
-	rowFive[std::rand() % 10] = "*" + allStarsNames[4];
-	rowSix[std::rand() % 10] = "*" + allStarsNames[5];
-	rowSeven[std::rand() % 10] = "*" + allStarsNames[6];
-	rowEight[std::rand() % 10] = "*" + allStarsNames[7];
-	rowNine[std::rand() % 10] = "*" + allStarsNames[8];
-	rowTen[std::rand() % 10] = "*" + allStarsNames[9];
+	rowOne[std::rand() % 10] = "*" + allStarsNames[0] + "(" + std::to_string(allStars[0].controllingFaction) + ")";
+	rowTwo[std::rand() % 10] = "*" + allStarsNames[1] + "(" + std::to_string(allStars[1].controllingFaction) + ")";
+	rowThree[std::rand() % 10] = "*" + allStarsNames[2] + "(" + std::to_string(allStars[2].controllingFaction) + ")";
+	rowFour[std::rand() % 10] = "*" + allStarsNames[3] + "(" + std::to_string(allStars[3].controllingFaction) + ")";
+	rowFive[std::rand() % 10] = "*" + allStarsNames[4] + "(" + std::to_string(allStars[4].controllingFaction) + ")";
+	rowSix[std::rand() % 10] = "*" + allStarsNames[5] + "(" + std::to_string(allStars[5].controllingFaction) + ")";
+	rowSeven[std::rand() % 10] = "*" + allStarsNames[6] + "(" + std::to_string(allStars[6].controllingFaction) + ")";
+	rowEight[std::rand() % 10] = "*" + allStarsNames[7] + "(" + std::to_string(allStars[7].controllingFaction) + ")";
+	rowNine[std::rand() % 10] = "*" + allStarsNames[8] + "(" + std::to_string(allStars[8].controllingFaction) + ")";
+	rowTen[std::rand() % 10] = "*" + allStarsNames[9] + "(" + std::to_string(allStars[9].controllingFaction) + ")";
 
 	for (int i = 0; i < rowOne.size(); i++) {
 		std::cout << rowOne[i];
@@ -97,4 +96,17 @@ void CreateStar() {
 	allStars.push_back(newStar);
 	allStarsNames.push_back(newStar.starName);
 
+}
+void TakeStar(int selectingFaction, std::string starSelection) {
+	for (int i = 0; i < allStarsNames.size(); i++) {
+		if (allStarsNames[i] == starSelection) {
+			allStars[i].controllingFaction = selectingFaction;
+		}
+	}
+
+
+}
+void DeclareFactionCapitol(int factionCapitolSelector) {
+	allStars[std::rand() % allStars.size()].controllingFaction = 1;
+	allStars[std::rand() % allStars.size()].controllingFaction = 2;
 }
