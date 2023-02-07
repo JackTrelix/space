79aa2bb76dd0c0796414e8dfffc4e795f1043977
Jacob Fewell <45701961+JackTrelix@users.noreply.github.com>
2023-02-07 12:25:25 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,20 @@
 #include "Header.h"
 
 int main() {
+	std::string playerFactionName;
+	char playerFactionLabel;
+
 	Galaxy MyGalaxy;
 	DrawBoard();
+	
+	std::cout << "Please name your empire: ";
+	std::cin >> playerFactionName;
+	std::cout << "Please label your empire with a single character: ";
+	std::cin >> playerFactionLabel;
+
+	Faction playerFaction(playerFactionName, playerFactionLabel);
+	Faction enemyFaction;
+
+
 	return 0;
 }
