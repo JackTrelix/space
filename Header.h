ea6f50173f8af6ff161dd796a2c5ffe7c49db04e
Jacob Fewell <45701961+JackTrelix@users.noreply.github.com>
2023-02-07 12:25:44 -0800

    Update Header.h

--- Header.h
+++ Header.h
@@ -2,13 +2,19 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+//horrible coding practices and cutting corners
+static std::vector<char> alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
 //function definitions
 std::string WordGenerator();
 
 void DrawBoard();
 void CreateStar();
+void TakeStar(std::string starSelection);
+void DeclareStarCapitols();
 
 //classes
+class Galaxy;
 class Star {
 private:
 	std::vector<std::string> starTypes = {"Red", "Blue", "Yellow", "Green"};
@@ -16,16 +22,23 @@ public:
 	std::string starName = WordGenerator();
 	int numOfPlanets = std::rand() % 6;
 	std::string starType = starTypes[std::rand() % 4];
+	int controllingFaction = 0;
+	int iHasStarbase = std::rand() % 2;
+	bool hasStarbase;
+
 
 	Star() {
-
+		if (iHasStarbase == 1) {
+			hasStarbase = true;
+		}
 	}
 
 
 };
 class Galaxy {
-
+	
 public:
+
 	Galaxy() {
 		int galacticProportions = 10;
 		while (galacticProportions > 0) {
@@ -33,4 +46,19 @@ public:
 			galacticProportions--;
 		}
 	}
+};
+class Faction {
+	
+public:
+	std::string factionCapitol;
+	std::string factionName;
+	char factionLabel;
+	Faction(std::string newFactionName = WordGenerator(), char newFactionLabel = alphabet[std::rand() % alphabet.size()]) {
+		factionName = newFactionName;
+		factionLabel = toupper(newFactionLabel);
+	}
+
+
+
+
 };
