780e261658f9e09ed21a8513213003127309745b
Jacob Fewell <45701961+JackTrelix@users.noreply.github.com>
2023-02-14 12:15:07 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,21 +2,29 @@
 #include <cstdlib>
 #include <vector>
 #include "Header.h"
+#include <time.h>
 
 int main() {
-	std::string playerFactionName;
-	char playerFactionLabel;
+	srand(time(NULL));
+	std::string player1Name;
+	std::string player2Name;
+	char player1Label;
+	char player2Label;
 
 	Galaxy MyGalaxy;
 	DrawBoard();
 
-	std::cout << "Please name your empire: ";
-	std::cin >> playerFactionName;
+	std::cout << "Player 1, please name your empire: ";
+	std::cin >> player1Name;
 	std::cout << "Please label your empire with a single character: ";
-	std::cin >> playerFactionLabel;
-
-	Player playerFaction1(playerFactionName, playerFactionLabel);
-	Player playerFaction2;
+	std::cin >> player1Label;
+	std::cout << "Player 2, please name your empire: ";
+	std::cin >> player2Name;
+	std::cout << "Please label your empire with a single character: ";
+	std::cin >> player2Label;
+	Player player1(playerName, playerLabel);
+	Player player2;
+	
 	DeclareFactionCapitols();
 	DrawBoard();
 	GameLoop();
